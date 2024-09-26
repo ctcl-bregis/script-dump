d99f60f4f7dba7f71a8dea7d0cdd8c90e461d6b7
ctcl-bregis <brayden.regis@gmail.com>
2024-09-26 18:27:02 -0400

    Sep 26, 2024 2

--- converters/8to6bit.py
+++ converters/8to6bit.py
@@ -1,6 +1,6 @@
 # 8-bit to 6-bit Color Depth Converter - CTCL 2024
 # Created: September 15, 2024
-# Modified: September 15, 2024
+# Modified: September 26, 2024
 
 # Formulas from: https://threadlocalmutex.com/?p=48
 
@@ -14,15 +14,23 @@ args = parser.parse_args()
 
 x = args.input
 
+# This script is for calculating the displayed equivalent RGB888 color on an RGB666 display being driven from an RGB888 interface where bits of each color on the RGB666 display is connected to bits 2-7 (out of 0-7) of each color channel on the RGB888 interface.
+# Basically, the first two bits of each 8-bit color value are ignored
+
 if args.type == "dec":
     x = int(args.input)
-    print((x * 253 + 512) >> 10)
+    x &=~ (1<<0)
+    x &=~ (1<<1)
+    print(x)
 elif args.type == "hex": 
     x = int(args.input, 16)
-    print((x * 253 + 512) >> 10)
+    x &=~ (1<<0)
+    x &=~ (1<<1)
+    print(x)
 elif args.type == "rgb":
     x = [int(args.input[i:i+2], 16) for i in range(0, len(args.input), 2)]
-    x = [((i * 253 + 512) >> 10) * 4 for i in x ] 
+    x = [i &~ (1<<1) for i in x] 
+    x = [i &~ (1<<0) for i in x] 
     x = [hex(i)[2:].ljust(2, "0") for i in x]
     print("".join(x))
    
